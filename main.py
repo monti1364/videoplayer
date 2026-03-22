af785850d6fe44ada3135efaed456c97c4ef409a
monti1364 <nirwanmonti42@gmail.com>
2026-03-22 20:47:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.videoplayer import VideoPlayer
+from kivy.core.clipboard import Clipboard
+import traceback
+
+KV = '''
+MDScreen:
+    VideoPlayer:
+        id: player
+        source: "assets/v1.mp4"
+        state: 'play'
+        options: {'allow_stretch': True}
+'''
+
+class VideoApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+if __name__ == "__main__":
+    try:
+        VideoApp().run()
+    except Exception as e:
+        error=traceback.format_exc()
+        Clipboard(str(error))                
+
 
