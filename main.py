178e54a33ed3aeecdeda51109134f2d3f94aa962
monti1364 <nirwanmonti42@gmail.com>
2026-03-22 21:50:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.videoplayer import VideoPlayer
 from kivy.core.clipboard import Clipboard
+from kivy.logger import Logger
 import traceback
 
+# KV String
 KV = '''
 MDScreen:
+    md_bg_color: 0, 0, 0, 1
     VideoPlayer:
         id: player
         source: "assets/v1.mp4"
@@ -15,13 +18,20 @@ MDScreen:
 
 class VideoApp(MDApp):
     def build(self):
-        return Builder.load_string(KV)
+        try:
+            return Builder.load_string(KV)
+        except Exception as e:
+            self.copy_error_to_clipboard(traceback.format_exc())
+
+    def copy_error_to_clipboard(self, error_text):
+        Logger.error(f"AppCrash: {error_text}")
+        Clipboard.copy(error_text)
 
 if __name__ == "__main__":
     try:
         VideoApp().run()
-    except Exception as e:
-        error=traceback.format_exc()
-        Clipboard.copy(str(error))                
-
+    except Exception:
+        error_msg = traceback.format_exc()
+        Clipboard.copy(error_msg)
+        Logger.error(f"CriticalCrash: {error_msg}")
         
